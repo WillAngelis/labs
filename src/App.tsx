36d75f91c1e18d50ccc9651d9e7f274a2a43aeed
Will Angelis <willangelis@gmail.com>
2022-10-15 23:05:28 -0300

    add configs for storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import './styles/global.css';
 
 export const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-black">
+        Logue na <span className="font-bold text-2xl text-green-500">Labs</span>
+      </h1>
+    </>
+  );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    fontSize: {
+      xxs: 12,
+      xs: 14,
+      sm: 16,
+      md: 24,
+      lg: 32,
+      xl: 42,
+      '2xl': 56,
+    },
+    colors: {
+      'green-500': '#73FE42',
+      'green-300': '#77FF85',
+      'green-200': '#B5FFB4',
+      'gray-900': '#202024',
+      'gray-700': '#252525',
+      'gray-500': '#7c7c8a',
+      transparent: 'transparent',
+      black: '#121214',
+      white: '#fff',
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Poppins',
+      },
+    },
   },
   plugins: [],
 };
